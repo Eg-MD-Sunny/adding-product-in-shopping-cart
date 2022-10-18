bf0b322f3cf24072af7818570a978debee7d5007
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-19 00:32:12 +0600

    Modify

--- src/Componenet/Product/Product.js
+++ src/Componenet/Product/Product.js
@@ -3,15 +3,18 @@ import './Product.css';
 
 const Product = (props) => {
     const {_id, name, price} = props.product;
+// >> Part 01    
     const addToCart = () =>{
         console.log('Product Added')
     }
+// << Part 01     
     return (
         <div className="singleProductStyle">
             <p>Id: {_id}</p>
             <p>Name: {name}</p>
             <p>Price: {price}</p>
-            <button onClick={addToCart}>Add To Cart</button>
+{/*>><< Part 01*/}
+            <button onClick={addToCart}>Add To Cart</button>            
         </div>
     );
 };
