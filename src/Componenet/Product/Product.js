a43f645e9b51fd5189d5aeb8736680ca127e30f6
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-19 00:29:03 +0600

    Modify

--- src/Componenet/Product/Product.js
+++ src/Componenet/Product/Product.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { addToCartDB } from '../Utilities/LocalDB';
 import './Product.css';
 
 const Product = (props) => {
-    const {_id, name, price} = props.product
-
-    const addToCart = (_id) =>{
-        // console.log('Product Added',_id);
-        // localStorage.setItem(_id,1);
-        addToCartDB(_id);
+    const {_id, name, price} = props.product;
+    const addToCart = () =>{
+        console.log('Product Added')
     }
-
-
     return (
         <div className="singleProductStyle">
             <p>Id: {_id}</p>
             <p>Name: {name}</p>
             <p>Price: {price}</p>
-            <button onClick={()=>addToCart(_id)}>Add To Cart</button>
+            <button onClick={addToCart}>Add To Cart</button>
         </div>
     );
 };
--- src/Componenet/Utilities/LocalDB.js
+++ src/Componenet/Utilities/LocalDB.js
@@ -1,16 +1,2 @@
-const addToCartDB = _id =>{
-   
-    const quantity = localStorage.getItem(_id)
-    // console.log(quantity);
-    if(quantity){
-        console.log('Already Exists')
-        const newQunatity = parseInt(quantity) + 1;
-        localStorage.setItem(_id,newQunatity);
-    }else{
-        console.log('New Item Added')
-        localStorage.setItem(_id,1)
-    }
-}
 
-export { addToCartDB };
 
