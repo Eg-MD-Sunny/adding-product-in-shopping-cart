678cd9a031ea7079f3bcf87f3368980764fde8df
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-20 23:39:25 +0600

    Modify

--- src/Componenet/Utilities/LocalDB.js
+++ src/Componenet/Utilities/LocalDB.js
@@ -1,6 +1,14 @@
 const storageBD = _id =>{
-    localStorage.setItem(_id,1)
+    // localStorage.setItem(_id,1)
+
+    const quantity = localStorage.getItem(_id)
+    if(quantity>0){
+        console.log("This Product Already Exists");
+    }else{
+        console.log('New Product Added');
+        localStorage.setItem(_id,1);
+    }
 }
 
-export { storageBD }
+export { storageBD };
 
